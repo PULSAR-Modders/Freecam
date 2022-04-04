51ef2c4f64e58673ca61ace1ce58cced51e1b8d4
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2022-04-03 21:49:59 -0700

    Add project files.

---
+++ Freecam/Command.cs
@@ -0,0 +1,29 @@
+﻿using PulsarModLoader.Chat.Commands.CommandRouter;
+
+namespace Freecam
+{
+    class Command : ChatCommand
+    {
+        public override string[] CommandAliases()
+        {
+            return new string[] { "freecam" };
+        }
+
+        public override string Description()
+        {
+            return "removes the camera from the local pawn";
+        }
+
+        public override void Execute(string arguments)
+        {
+            if (PLCameraSystem.Instance.CurrentCameraMode != null && PLCameraSystem.Instance.CurrentCameraMode.GetType() == typeof(PLCameraMode_Scripted))
+            {
+                PLCameraSystem.Instance.ChangeCameraMode(new PLCameraMode_LocalPawn());
+            }
+            else
+            {
+                PLCameraSystem.Instance.ChangeCameraMode(new PLCameraMode_Scripted());
+            }
+        }
+    }
+}
---
+++ Freecam/Mod.cs
@@ -0,0 +1,18 @@
+﻿using PulsarModLoader;
+
+namespace Freecam
+{
+    public class Mod : PulsarMod
+    {
+        public override string Version => "0.0.1";
+
+        public override string Author => "Dragon";
+
+        public override string Name => "Rename Me";
+
+        public override string HarmonyIdentifier()
+        {
+            return $"{Author}.{Name}";
+        }
+    }
+}
